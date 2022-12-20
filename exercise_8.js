0c1dfb56ea803122497280e6309f096032b5f82b
5yga <budadorzhiev@gmail.com>
2022-12-20 21:21:23 +0800

    redid the exercise

--- exercise_8.js
+++ exercise_8.js
@@ -1,5 +1,3 @@
-let keyArray = [];
-let valueArray = [];
 let ourMap = new Map();
 
 ourMap.set('3.14', ';P');
@@ -7,14 +5,7 @@ ourMap.set(20, 'twenty');
 ourMap.set(false, 'ten');
 ourMap.set('true', 24);
 
-for (let key of ourMap.keys()){
-  keyArray.push(key);
-}
-
-for (let value of ourMap.values()){
-  valueArray.push(value);
-}
-
-for (let i = 0; i < ourMap.size; i++){
-  console.log(`Ключ - ${keyArray[i]}, значение - ${valueArray[i]}`);
+for (let pair of ourMap) {
+  // pair - это массив [key, values]
+  console.log(`Ключ = ${pair[0]}, значение = ${pair[1]}`);
 }
